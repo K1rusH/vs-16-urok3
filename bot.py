209663d782ae759eaf012fb3b3d4dcaaa7ec652d
K1rusH <68894107+K1rusH@users.noreply.github.com>
2024-06-16 17:19:55 +0300

    Update bot.py
    Я передумал, вернул принт обратно

--- bot.py
+++ bot.py
@@ -8,6 +8,8 @@ def start(message):
     bot.reply_to(message, "Привет! Я бот для управления чатом.")
 
 
+print("Новый принт")
+
 @bot.message_handler(commands=['ban'])
 def ban_user(message):
     if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
